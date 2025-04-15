fbc0f42861725bff53c8a9e07038507f0a697152
zaynabali-12345 <zaynabali00786@gmail.com>
2025-04-15 14:23:29 +0530

    Message

---
+++ SimpleCalculator.java
@@ -0,0 +1,10 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        int num1=2,num2 = 10;
+        System.out.println("Addition:"+(num1+num2));
+        System.out.println("Substraction:"+(num1-num2));
+        System.out.println("Multiplication:"+(num1*num2));
+        System.out.println("Division:"+(num1/num2));
+        System.out.println("Modulus:"+(num1%num2));
+    }
+}
