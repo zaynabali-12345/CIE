a26712ce00346632bfad1bb4f3c9f9b3ab5dd7ad
zaynabali-12345 <zaynabali00786@gmail.com>
2025-04-15 14:42:31 +0530

    Save to output branch

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -6,5 +6,6 @@ public class SimpleCalculator {
         System.out.println("Multiplication:"+(num1*num2));
         System.out.println("Division:"+(num1/num2));
         System.out.println("Modulus:"+(num1%num2));
+        System.out.println("End of the program");
     }
 }
