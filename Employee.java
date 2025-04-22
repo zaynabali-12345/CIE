73e48c066bfab22a7102400aa278f226dea4094c
zaynabali-12345 <zaynabali00786@gmail.com>
2025-04-22 14:04:55 +0530

    Message

---
+++ Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+    public static void main(String[] args) {
+        String name = "xyz";
+        int id = 30;
+        int salary =200000;
+        String company = "xyz";
+
+        System.out.println("---------Employee Details----------");
+        System.out.println("Employee Name:"+name);
+        System.out.println("Employee ID:"+id);
+        System.out.println("Employee Salary:"+salary);
+        System.out.println("Employee Company:"+company);
+    }
+}
